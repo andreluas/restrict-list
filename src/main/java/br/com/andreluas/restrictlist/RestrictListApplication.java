e7938d1df8d87a35416bd71825c62af36431afe0
andreluas <andreluas.k@gmail.com>
2023-04-03 18:34:53 -0300

    chore: initial commit

---
+++ src/main/java/br/com/andreluas/restrictlist/RestrictListApplication.java
@@ -0,0 +1,13 @@
+package br.com.andreluas.restrictlist;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestrictListApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RestrictListApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/andreluas/restrictlist/RestrictListApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.andreluas.restrictlist;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestrictListApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
