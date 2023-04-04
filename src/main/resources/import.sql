8c09fbdd820c37afc3a5d2baa0ebf9e0c28c30cf
andreluas <andreluas.k@gmail.com>
2023-04-04 13:48:23 -0300

    chore: updated database import

--- src/main/resources/import.sql
+++ src/main/resources/import.sql
@@ -1,3 +1,3 @@
-INSERT INTO tb_restrict_list (id, cpf, created_at) VALUES ('82d6c4a5-042f-4751-92ed-6486a9ce2e3c', '824.310.540-98', '1996-10-22')
-INSERT INTO tb_restrict_list (id, cpf, created_at) VALUES ('856173d6-d26c-11ed-afa1-0242ac120002', '846.848.790-20', '1996-05-10')
-INSERT INTO tb_restrict_list (id, cpf, created_at) VALUES ('8a76099a-d26c-11ed-afa1-0242ac120002', '969.796.700-85', '1998-11-02')
+INSERT INTO tb_restrict_list (id, cpf, created_at) VALUES ('82d6c4a5-042f-4751-92ed-6486a9ce2e3c', '21198683015', '2023-04-04T14:54:37.433606Z')
+INSERT INTO tb_restrict_list (id, cpf, created_at) VALUES ('856173d6-d26c-11ed-afa1-0242ac120002', '68638275064', '2023-04-03T14:54:37.433606Z')
+INSERT INTO tb_restrict_list (id, cpf, created_at) VALUES ('8a76099a-d26c-11ed-afa1-0242ac120002', '89948320093', '2023-04-04T14:54:37.433606Z')
